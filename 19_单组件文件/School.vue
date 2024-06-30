0b60bf196bcc087a43b3d75686aeb4232c83a201
Enikish <www.1374915762@foxmail.com>
2024-06-30 22:34:42 +0800

    2024/6/30

---
+++ 19_单组件文件/School.vue
@@ -0,0 +1,34 @@
+<template>
+    <!-- 组件的结构 -->
+    <div id="root">
+        <h2>学校名称:{{ schoolName }}</h2>
+        <h2>学校地址:{{ address }}</h2>
+        <button @click="showName">点我提示学校名</button>
+    </div>
+
+</template>
+
+<script>
+    // 组件交互相关的代码
+    export default Vue.extend({
+        name:'School',
+        data() {
+            return {
+                schoolName: '尚硅谷',
+                address: '北京昌平',
+            }
+        },
+        methods: {
+            showName(){
+                alert(this.schoolName)
+            },
+        },
+    })
+</script>
+
+<style>
+    /* 组件的样式i */
+    .demo{
+        background-color: orange;
+    }
+</style>
---
+++ 19_单组件文件/Student.vue
@@ -0,0 +1,34 @@
+<template>
+    <!-- 组件的结构 -->
+    <div id="root">
+        <h2>学生姓名:{{ name }}</h2>
+        <h2>学生年龄:{{ age }}</h2>
+        <button @click="showName">点我提示学生姓名</button>
+    </div>
+
+</template>
+
+<script>
+    // 组件交互相关的代码
+    export default Vue.extend({
+        name:'Student',
+        data() {
+            return {
+                name: '张三',
+                age: 18,
+            }
+        },
+        methods: {
+            showName(){
+                alert(this.name)
+            },
+        },
+    })
+</script>
+
+<style>
+    /* 组件的样式i */
+    .demo{
+        background-color: purple;
+    }
+</style>
